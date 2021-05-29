842ebe366d8c5ea10ee702c83c1745eb05183746
Антон Телышев <anton.telishev@yandex.ru>
2021-05-29 09:09:55 +0300

    add go.mod & taskfile

--- examples/02-c-errors-concept/03-errors-conception-in-go/02-errors-conception-in-go/byte-buffer-with-errors/byte_buffer_with_errors.go
+++ examples/02-c-errors-concept/03-errors-conception-in-go/02-errors-conception-in-go/byte-buffer-with-errors/byte_buffer_with_errors.go
@@ -10,7 +10,7 @@ func (e *MaxSizeExceededError) Error() string {
 	return fmt.Sprintf("max allowed length 1024 is less than desired %d", e.desiredLen)
 }
 
-type BufferIsEmptyError struct {}
+type BufferIsEmptyError struct{}
 
 func (b *BufferIsEmptyError) Error() string {
 	return "buffer is empty"
@@ -22,7 +22,7 @@ type ByteBuffer struct {
 }
 
 func (b *ByteBuffer) Write(p []byte) (int, error) {
-	if len(b.buffer) + len(p) > 1024 {
+	if len(b.buffer)+len(p) > 1024 {
 		return 0, &MaxSizeExceededError{desiredLen: len(b.buffer) + len(p)}
 	}
 
@@ -34,7 +34,7 @@ func (b *ByteBuffer) Read(p []byte) (int, error) {
 	if len(b.buffer) == b.offset {
 		return 0, &BufferIsEmptyError{}
 	}
-	
+
 	n := copy(p, b.buffer[b.offset:])
 	b.offset += n
 	return n, nil
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/www-golang-courses-ru/advanced-dealing-with-errors-in-go
+
+go 1.16
+
+require github.com/stretchr/testify v1.7.0
