59f738fa3e890ad01aa730b8711152b46d127a79
Антон Телышев <anton.telishev@yandex.ru>
2021-06-13 08:43:10 +0300

    ErrWithTime -> WithTimeError

--- tasks/03-go-errors-concept/with-time-error/with_time_error_test.go
+++ tasks/03-go-errors-concept/with-time-error/with_time_error_test.go
@@ -10,33 +10,33 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func ExampleErrorWithTime() {
-	err := NewErrorWithTime(context.Canceled)
+func ExampleWithTimeError() {
+	err := NewWithTimeError(context.Canceled)
 	fmt.Println(err)
 	// context canceled, occurred at: 2021-06-07T08:15:48.518835+03:00
 }
 
-func TestErrorWithTime(t *testing.T) {
+func TestWithTimeError(t *testing.T) {
 	var s int64
 	timeNowMock := func() time.Time {
 		s++
 		return time.Unix(s, 0)
 	}
 
-	newErrorWithTimeMock := func(err error) error {
-		return newErrorWithTime(err, timeNowMock)
+	newWithTimeErrorMock := func(err error) error {
+		return newWithTimeError(err, timeNowMock)
 	}
 
 	var timed interface {
 		Time() time.Time
 	}
 
-	err := newErrorWithTimeMock(context.Canceled)
+	err := newWithTimeErrorMock(context.Canceled)
 	require.ErrorIs(t, err, context.Canceled)
 	require.ErrorAs(t, err, &timed)
 	require.Equal(t, 1, timed.Time().Second())
 
-	err = fmt.Errorf("cannot read file: %w", newErrorWithTimeMock(io.ErrUnexpectedEOF))
+	err = fmt.Errorf("cannot read file: %w", newWithTimeErrorMock(io.ErrUnexpectedEOF))
 	require.ErrorIs(t, err, io.ErrUnexpectedEOF)
 	require.ErrorAs(t, err, &timed)
 	require.Equal(t, 2, timed.Time().Second())
