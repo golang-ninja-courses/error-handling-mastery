1ee4e4b7efced6a033e71f414c8b61d3373fd719
Антон Телышев <anton.telishev@yandex.ru>
2021-06-27 09:44:37 +0300

    error types

---
+++ tasks/03-go-errors-concept/handling-error-types/handler.go
@@ -0,0 +1,41 @@
+package queue
+
+import (
+	"io"
+	"time"
+)
+
+const defaultPostpone = time.Second
+
+type Job struct {
+	ID int
+}
+
+type Handler struct{}
+
+func (h *Handler) Handle(job Job) (postpone time.Duration, err error) {
+	err = h.process(job)
+	if err != nil {
+		// Обработайте ошибку.
+	}
+
+	return 0, nil
+}
+
+func (h *Handler) process(job Job) error {
+	switch job.ID {
+	case 1:
+		return &InconsistentDataError{}
+	case 2:
+		return &NotReadyError{}
+	case 3:
+		return &NotFoundError{}
+	case 4:
+		return &AlreadyDoneError{}
+	case 5:
+		return &InvalidIDError{}
+	case 6:
+		return io.EOF
+	}
+	return nil
+}
