65f2db1562358f314823210cea68139206a8e7f9
Антон Телышев <anton.telishev@yandex.ru>
2021-08-27 07:53:15 +0300

    cockroach-any

--- examples/04-non-standard-modules/cockroach-is-any/main.go
+++ examples/04-non-standard-modules/cockroach-is-any/main.go
@@ -10,7 +10,13 @@ import (
 )
 
 func main() {
-	errs := []error{context.Canceled, io.EOF, os.ErrClosed, os.ErrNotExist, errors.New("unknown error")}
+	errs := []error{
+		context.Canceled,
+		io.EOF,
+		os.ErrClosed,
+		os.ErrNotExist,
+		errors.New("unknown error"),
+	}
 
 	for _, err := range errs {
 		if errors.IsAny(err, errs...) {
