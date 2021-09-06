601ad3744b649021f1b1a7419fe87ab7737bd6c7
Антон Телышев <anton.telishev@yandex.ru>
2021-09-06 08:22:05 +0300

    error printing

---
+++ examples/08-future-of-errors-in-go2/copy-file/v1.go
@@ -0,0 +1,14 @@
+package example
+
+// Несуществующий синтаксис.
+
+/*
+func CopyFileV1(src, dst string) throws error {
+	r := os.Open(src)
+	defer r.Close()
+
+	w := os.Create(dst)
+	io.Copy(w, r)
+	w.Close()
+}
+*/
