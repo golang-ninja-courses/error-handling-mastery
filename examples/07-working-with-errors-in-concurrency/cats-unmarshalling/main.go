3de764b89c3fce75e0ae989728b56cce49dd87be
Антон Телышев <anton.telishev@yandex.ru>
2021-09-22 19:29:52 +0300

    cats-unmarshalling

---
+++ examples/07-working-with-errors-in-concurrency/cats-unmarshalling/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"sync"
+)
+
+type Cat struct {
+	Name string `json:"name"`
+}
+
+func main() {
+	catsJSONs := []string{`{"name": "Bobby"}`, `"name": "Billy"`, `{"name": "Васёк"}`}
+	catsCh := make(chan Cat, len(catsJSONs))
+
+	var wg sync.WaitGroup
+	wg.Add(len(catsJSONs))
+
+	for _, catData := range catsJSONs {
+		go func(catData string) { // Разбираем котиков в несколько горутин.
+			defer wg.Done()
+
+			var cat Cat
+			if err := json.Unmarshal([]byte(catData), &cat); err != nil {
+				// Случилась ошибка, что делать?
+			}
+			catsCh <- cat
+		}(catData)
+	}
+
+	go func() {
+		wg.Wait()
+		close(catsCh)
+	}()
+
+	for cat := range catsCh {
+		fmt.Println(cat)
+	}
+}
