80bbadf4ab3bf8f52536fd12028e1a1542dfc23f
Антон Телышев <anton.telishev@yandex.ru>
2022-01-25 17:09:57 +0300

    examples/04-non-standart-modules/cockroach-with-secondary-error: cosmetic

--- examples/04-non-standard-modules/cockroach-with-secondary-error/main.go
+++ examples/04-non-standard-modules/cockroach-with-secondary-error/main.go
@@ -12,6 +12,11 @@ func main() {
 	err := io.EOF
 	err = errors.WithSecondaryError(err, context.Canceled)
 
+	fmt.Println(err)
+	/*
+		EOF
+	*/
+
 	fmt.Printf("%+v", err)
 	/*
 		EOF
@@ -20,9 +25,10 @@ func main() {
 		  | (1) context canceled
 		  | Error types: (1) *errors.errorString
 		Wraps: (2) EOF
-		Error types: (1) *secondary.withSecondaryError (2) *errors.errorStringtrue
+		Error types: (1) *secondary.withSecondaryError (2) *errors.errorString
 	*/
 
+	fmt.Println()
 	fmt.Println(errors.Is(err, io.EOF))           // true
 	fmt.Println(errors.Is(err, context.Canceled)) // false
 }
