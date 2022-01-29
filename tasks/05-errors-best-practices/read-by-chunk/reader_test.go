db384a12a6952262260506a51c5716f2cb375b49
Антон Телышев <anton.telishev@yandex.ru>
2022-01-29 21:04:57 +0300

    tasks/05-errors-best-practices/read-by-chunk: greedy reader test

--- tasks/05-errors-best-practices/read-by-chunk/reader_test.go
+++ tasks/05-errors-best-practices/read-by-chunk/reader_test.go
@@ -74,6 +74,16 @@ func TestReadByChunk(t *testing.T) {
 			},
 		},
 		{
+			// If some data is available but not len(p) bytes, Read conventionally
+			// returns what is available instead of waiting for more.
+			name:      "greedy reader giving out by 5 bytes to 7 bytes chunk",
+			r:         newGreedyReader(s, 5),
+			chunkSize: 7,
+			expectedChunks: [][]byte{
+				{'E', 'r', 'r', 'o', 'r', 's', ' '}, {'a', 'r', 'e', ' ', 'v', 'a', 'l'}, {'u', 'e', 's', '.'},
+			},
+		},
+		{
 			name:      "one chunk equals to source",
 			r:         strings.NewReader(s),
 			chunkSize: len(s),
@@ -107,12 +117,70 @@ func TestReadByChunk(t *testing.T) {
 	}
 }
 
+func Test_greedyReader(t *testing.T) {
+	const (
+		wantToRead      = 30
+		maxBytesPerRead = 10
+	)
+
+	r := newGreedyReader("Errors are values.", maxBytesPerRead)
+	{
+		data := make([]byte, wantToRead)
+		n, err := r.Read(data)
+		assert.NoError(t, err)
+		assert.Equal(t, 10, n)
+		assert.Equal(t, "Errors are", string(data[:n]))
+	}
+	{
+		data := make([]byte, wantToRead)
+		n, err := r.Read(data)
+		assert.NoError(t, err)
+		assert.Equal(t, 8, n)
+		assert.Equal(t, " values.", string(data[:n]))
+	}
+	{
+		data := make([]byte, wantToRead)
+		n, err := r.Read(data)
+		assert.ErrorIs(t, err, io.EOF)
+		assert.Equal(t, 0, n)
+		assert.Equal(t, make([]byte, wantToRead), data)
+	}
+}
+
 type errReader struct {
 	err error
 }
 
 func (r errReader) Read([]byte) (int, error) {
 	return 0, r.err
+}
+
+// greedyReader представляет собой жадного читателя,
+// т.е. выдающего не более чем maxBytesPerRead байт за раз.
+type greedyReader struct {
+	data            []byte
+	maxBytesPerRead int
+	pos             int
+}
+
+func newGreedyReader(str string, maxPerRead int) *greedyReader {
+	return &greedyReader{data: []byte(str), maxBytesPerRead: maxPerRead}
+}
+
+func (s *greedyReader) Read(p []byte) (int, error) {
+	if s.pos >= len(s.data) {
+		return 0, io.EOF
+	}
+
+	right := s.pos + s.maxBytesPerRead
+	if right > len(s.data) {
+		right = len(s.data)
+	}
+
+	n := copy(p, s.data[s.pos:right])
+	s.pos += n
+
+	return n, nil
 }
 
 func debugChunks(chunks [][]byte) string {
