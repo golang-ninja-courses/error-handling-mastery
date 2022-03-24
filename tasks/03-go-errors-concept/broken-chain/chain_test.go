e0d16e955510a73822877055b9abf188a3eefd81
Антон Телышев <anton.telishev@yandex.ru>
2022-03-24 10:19:54 +0300

    tasks/03-go-errors-concept/broken-chain: NotErrorIs

--- tasks/03-go-errors-concept/broken-chain/chain_test.go
+++ tasks/03-go-errors-concept/broken-chain/chain_test.go
@@ -24,6 +24,7 @@ func TestProcessMessage(t *testing.T) {
 	err := ProcessMessage()
 	assert.Error(t, err)
 	assert.ErrorIs(t, err, io.ErrShortWrite)
+	assert.NotErrorIs(t, err, io.EOF)
 	assert.EqualError(t, err,
 		`cannot process msg: cannot write data: save msg "8fbad38c-c5c5-11eb-b876-1e00d13a7870" error: short write`)
 }
