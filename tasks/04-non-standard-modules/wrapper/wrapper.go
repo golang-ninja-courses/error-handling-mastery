e747057cb895ee16cbb172dbf5bfbc61b8033c83
Антон Телышев <anton.telishev@yandex.ru>
2022-06-22 08:36:14 +0300

    tasks/04-non-standard-modules/wrapper: rewrite comment

--- tasks/04-non-standard-modules/wrapper/wrapper.go
+++ tasks/04-non-standard-modules/wrapper/wrapper.go
@@ -2,5 +2,5 @@ package errors
 
 // Wrapper требует от типа быть ошибкой, поддерживающей API
 // как стандартной библиотеки, так и github/pkg/errors.
-type Wrapper interface { // Опиши меня.
+type Wrapper interface { // Добавь интерфейсу методов.
 }
