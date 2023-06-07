a8e99293b5f364b2ff2a25cd3b58fba2d7e0f31a
Anton Telyshev <anton.telishev@yandex.ru>
2023-06-07 10:07:31 +0300

    tasks/03-go-errors-concept/extract

---
+++ tasks/03-go-errors-concept/extract/extract.go
@@ -0,0 +1,8 @@
+package errs
+
+// Extract достаёт из цепочки err набор sentinel-ошибок,
+// игнорируя "оборачивающие" их ошибки.
+func Extract(err error) []error {
+	// Реализуй меня.
+	return nil
+}
---
+++ tasks/03-go-errors-concept/extract/extract_test.go
@@ -0,0 +1,54 @@
+package errs
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestChain(t *testing.T) {
+	errInvalidCursor := errors.New("invalid cursor")
+
+	cases := []struct {
+		in       error
+		expected []error
+	}{
+		{
+			in:       io.EOF,
+			expected: []error(nil),
+		},
+		{
+			in:       fmt.Errorf("parse request: %w", errInvalidCursor),
+			expected: []error{errInvalidCursor},
+		},
+		{
+			in:       fmt.Errorf("parse request: %w: %w", errInvalidCursor, io.EOF),
+			expected: []error{errInvalidCursor, io.EOF},
+		},
+		{
+			in: fmt.Errorf("parse request: %w: %w: %w: %w",
+				context.Canceled,
+				fmt.Errorf("%w", errInvalidCursor),
+				fmt.Errorf("another yet error: %w: %w", io.EOF, context.DeadlineExceeded),
+				nil,
+			),
+			expected: []error{
+				context.Canceled,
+				errInvalidCursor,
+				io.EOF,
+				context.DeadlineExceeded,
+			},
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run("", func(t *testing.T) {
+			chain := Extract(tt.in)
+			assert.Equal(t, tt.expected, chain)
+		})
+	}
+}
