577c9e002c67b07f9bd6b15a18654eac072baf91
Egor Zhuchkov <eazhuchkov@avito.ru>
2023-06-28 23:53:45 +0400

    tasks/03-go-errors-concept/errors-is: update test for false-positive

--- tasks/03-go-errors-concept/errors-is/pipeline_err_test.go
+++ tasks/03-go-errors-concept/errors-is/pipeline_err_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func TestPipelineError_Is(t *testing.T) {
-	users := []string{"Bob", "John"}
+	users := []string{"Bob", "John", ""}
 	operations := []string{"bitcoin calculation", "file downloading"}
 	steps := []string{"init", "hash", "download", "save"}
 
