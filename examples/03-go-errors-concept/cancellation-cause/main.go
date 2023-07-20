73b6bebeaac45999d9f2722463db8e77f70f9e4c
Anton Telyshev <anton.telishev@yandex.ru>
2023-07-20 07:15:29 +0300

    examples/03-go-errors-concept/cancellation-cause

---
+++ examples/03-go-errors-concept/cancellation-cause/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var (
+	errProgramInterrupted   = errors.New("program interrupted")
+	errOrchestrationTimeout = errors.New("orchestration timeout")
+	errWorkTimeout          = errors.New("work timeout")
+)
+
+func main() {
+	ctx, cancel := context.WithCancelCause(context.Background())
+	defer cancel(nil)
+
+	go func() {
+		ch := make(chan os.Signal)
+		signal.Notify(ch, os.Interrupt)
+		<-ch
+		cancel(errProgramInterrupted)
+	}()
+
+	err := orchestrate(ctx)
+	fmt.Println(err)
+	// aborting work: context canceled, because of program interrupted
+	// or
+	// aborting work: context deadline exceeded, because of orchestration timeout
+}
+
+func orchestrate(ctx context.Context) error {
+	ctx, cancel := context.WithTimeoutCause(ctx, 3*time.Second, errOrchestrationTimeout)
+	defer cancel()
+	return doWork(ctx)
+}
+
+func doWork(ctx context.Context) error {
+	ctx, cancel := context.WithTimeoutCause(ctx, 10*time.Second, errWorkTimeout)
+	defer cancel()
+
+	<-ctx.Done()
+	return fmt.Errorf("aborting work: %v, because of %w", ctx.Err(), context.Cause(ctx))
+}
