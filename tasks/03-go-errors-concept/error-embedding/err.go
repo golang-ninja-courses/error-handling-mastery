d9e79da210d1a88d103360854f7c01ba4043f162
VTB217707 <tarasov.ms@dev.vtb>
2024-10-16 22:12:13 +0400

    solve embedding

--- tasks/03-go-errors-concept/error-embedding/err.go
+++ tasks/03-go-errors-concept/error-embedding/err.go
@@ -1,5 +1,35 @@
 package errors
 
+
+type Err struct {
+	Msg string
+}
+
+type AlreadyDoneError struct {
+	coreErr Err
+}
+func (e *AlreadyDoneError) Error() string { return "job is already done" }
+
+type InconsistentDataError struct {
+	coreErr Err
+}
+func (e *InconsistentDataError) Error() string { return "job payload is corrupted" }
+
+type InvalidIDError struct {
+	coreErr Err
+}
+func (e *InvalidIDError) Error() string { return "invalid job id" }
+
+type NotReadyError struct {
+	coreErr Err
+}
+func (e *NotFoundError) Error() string { return "job wasn't found"}
+
+type NotFoundError struct {
+	coreErr Err
+}
+func (e *NotReadyError) Error() string { return "job is not ready to be performed"}
+
 var (
 	ErrAlreadyDone      error = &AlreadyDoneError{Err{"job is already done"}}
 	ErrInconsistentData error = &InconsistentDataError{Err{"job payload is corrupted"}}
@@ -7,5 +37,6 @@ var (
 	ErrNotReady         error = &NotReadyError{Err{"job is not ready to be performed"}}
 	ErrNotFound         error = &NotFoundError{Err{"job wasn't found"}}
 )
+
 
 // Реализуй тип Err и типы для ошибок выше, используя его.
