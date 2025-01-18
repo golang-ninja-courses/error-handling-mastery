2d3925bc9a625e75604831b0b768b7a8ef7af9cc
raptor72 <mctaras51@yandex.ru>
2025-01-18 15:10:41 +0400

    solve err-as

--- tasks/03-go-errors-concept/errors-as/pipeline_err.go
+++ tasks/03-go-errors-concept/errors-as/pipeline_err.go
@@ -21,4 +21,31 @@ func (p *PipelineError) Error() string {
 	return fmt.Sprintf("pipeline %q error", p.Name)
 }
 
-// Добавь метод As для типа *PipelineError.
+func (u *UserError) Redirect(t *PipelineError) {
+	//	*(&u.Operation) = t.Name
+	//	*(&u.User) = t.User
+	// x := *t
+	// uO := u.Operation
+	// u.User = x.User
+	// uO = x.Name
+	// u.Operation = x.Name
+	// u.User = t.User
+	// t.Name = u.Operation // wrong
+	// newHead := &PipelineError{u.User, u.Operation, nil}
+	// *u = newHead
+}
+
+func (p *PipelineError) As(target any) bool {
+	// if target == nil {
+	// 	return false
+	// }
+	f2, ok := target.(**UserError)
+	if ok {
+		*f2 = &UserError{
+			Operation: p.Name,
+			User:      p.User,
+		}
+		return true
+	}
+	return false
+}
