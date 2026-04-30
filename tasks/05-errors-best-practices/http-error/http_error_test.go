4ec4b3de31c1269d15be3bcb65c5ab42e679e570
Anton Telyshev <anton.telishev@yandex.ru>
2026-04-30 15:15:23 +0300

    tasks/05-errors-best-practices/http-error: fix exp actual places

--- tasks/05-errors-best-practices/http-error/http_error_test.go
+++ tasks/05-errors-best-practices/http-error/http_error_test.go
@@ -10,34 +10,34 @@ import (
 
 func TestHTTPError(t *testing.T) {
 	cases := []struct {
-		err  error
-		code int
-		text string
+		err     error
+		code    int
+		expText string
 	}{
 		{
-			err:  ErrStatusOK,
-			code: http.StatusOK,
-			text: "200 OK",
+			err:     ErrStatusOK,
+			code:    http.StatusOK,
+			expText: "200 OK",
 		},
 		{
-			err:  ErrStatusBadRequest,
-			code: http.StatusBadRequest,
-			text: "400 Bad Request",
+			err:     ErrStatusBadRequest,
+			code:    http.StatusBadRequest,
+			expText: "400 Bad Request",
 		},
 		{
-			err:  ErrStatusNotFound,
-			code: http.StatusNotFound,
-			text: "404 Not Found",
+			err:     ErrStatusNotFound,
+			code:    http.StatusNotFound,
+			expText: "404 Not Found",
 		},
 		{
-			err:  ErrStatusUnprocessableEntity,
-			code: http.StatusUnprocessableEntity,
-			text: "422 Unprocessable Entity",
+			err:     ErrStatusUnprocessableEntity,
+			code:    http.StatusUnprocessableEntity,
+			expText: "422 Unprocessable Entity",
 		},
 		{
-			err:  ErrStatusInternalServerError,
-			code: http.StatusInternalServerError,
-			text: "500 Internal Server Error",
+			err:     ErrStatusInternalServerError,
+			code:    http.StatusInternalServerError,
+			expText: "500 Internal Server Error",
 		},
 	}
 
@@ -50,7 +50,7 @@ func TestHTTPError(t *testing.T) {
 		}
 		require.ErrorAs(t, tt.err, &c)
 		assert.Equal(t, tt.code, c.Code())
-		assert.Equal(t, tt.err.Error(), tt.text)
+		assert.Equal(t, tt.expText, tt.err.Error())
 	}
 }
 
